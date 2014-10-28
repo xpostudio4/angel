5949cb2a88aa908fce64c96321ad26dd25bcbfe1
Vivian Guillen <vivianguillen@Vivians-MacBook-Air.local>
2014-10-28 14:03:21 -0400

    added scroll menu and about arrow

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,36 @@
 /* Project specific Javascript goes here. */
+
+$(document).ready (function () { 
+
+
+	$( "#cover" ).each(function(){
+	    var $this = $(this);
+	    $this.css({'min-height':($(window).height())+'px'});
+
+	    /*Recalculate on window resize*/
+	    $(window).resize(function(){
+	        $this.css({'min-height':($(window).height())+'px'});
+	    });
+	});
+
+	$("#button").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#about").offset().top
+	   		}, 1000);
+	});
+
+	$("#first").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#cover").offset().top
+	   		}, 1000);
+	});
+
+	$("#second").click(function() {
+	    $('html, body').animate({
+	        scrollTop: $("#about").offset().top
+	   		}, 1000);
+	});
+
+	$('body').scrollspy({ target: '.navbar-example' })
+
+});
